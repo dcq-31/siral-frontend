ae337584d374c22a27cd354370d76c7298d6b88e
dcq <dariancapoteq@gmail.com>
2022-07-01 21:47:57 -0400

    Create Main Header and add it to Main Layout

---
+++ src/components/layout/MainHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <q-header class="bg-primary" elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-btn
+            label="SiRAl-UCLV"
+            class="text-body1 text-weight-bold"
+            padding="xs sm"
+            to="/"
+            flat
+            unelevated
+            no-caps
+          />
+        </q-toolbar-title>
+        <q-btn label="Cuenta" icon="person" to="/account" flat dense no-caps />
+      </q-toolbar>
+
+      <q-tabs
+        no-caps
+        dense
+        align="justify"
+        indicator-color="white"
+        :breakpoint="0"
+      >
+        <q-route-tab name="reserve" to="/reserve" label="Reserva" />
+        <q-route-tab name="statistics" to="/statistics" label="Estadísticas" />
+      </q-tabs>
+    </q-header>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainHeader',
+});
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,8 +1,6 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <MainNavbar v-model:drawer-state="drawer_state" />
-    <MainDrawer v-model:drawer-state="drawer_state" />
-
+    <MainHeader />
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -11,14 +9,12 @@
 
 <script lang="ts">
 import { ref, defineComponent } from 'vue';
-import MainNavbar from 'components/layout/MainNavbar.vue';
-import MainDrawer from 'components/layout/MainDrawer.vue';
+import MainHeader from 'components/layout/MainHeader.vue';
 
 export default defineComponent({
   name: 'MainLayout',
   components: {
-    MainNavbar,
-    MainDrawer,
+    MainHeader,
   },
   setup() {
     const drawer_state = ref(false);

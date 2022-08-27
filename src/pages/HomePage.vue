36b8c1019c27a801812ddeffa6b5ecaaa6c79631
dcq <dariancapoteq@gmail.com>
2022-08-27 09:20:30 -0400

    Improve design, stucture and style of Home Page.

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,91 +1,94 @@
 <template>
   <q-page padding>
-    <div class="q-pb-md q-pt-xl text-center">
-      <section class="q-gutter-y-lg q-pt-sm q-pb-xl">
-        <div class="row">
-          <div class="col-12 col-sm-4 q-mb-md">
-            <q-img src="logo.png" alt="SIRAl logo" width="130px" fit="fill" />
+    <!-- Banner -->
+    <section class="text-center">
+      <div class="row">
+        <div class="col-10 col-md-7 col-xl-8 q-mx-auto q-gutter-y-lg">
+          <q-img src="logo.png" alt="SIRAl logo" width="145px" fit="fill" />
+          <div class="text-h4 text-weight-bold text-gradient-primary">
+            Sistema de Reservación de Alimentación
           </div>
-          <div class="col-12 col-md-8">
-            <div class="text-h4 text-weight-medium text-primary-1">
-              Sistema de Reservación de Alimentación
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-12">
+
+          <div>
             <q-btn
               color="primary"
-              size="16px"
               label="Menú de hoy"
-              padding="sm md"
+              padding="10px lg"
+              :ripple="false"
               unelevated
               no-caps
-              class="text-body1"
+              class="text-body2 text-weight-medium q-mx-auto"
             />
           </div>
         </div>
-      </section>
+      </div>
+    </section>
+    <!-- / Banner -->
 
-      <section class="q-py-xl q-px-md bg-blue-1">
-        <div class="row">
-          <div class="col-12 col-sm-4 q-gutter-y-sm text-center">
-            <div class="text-h5 text-weight-medium">
-              Planifica tu calendario
-            </div>
-            <div class="text-body2 q-mb-sm">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
-              numquam necessitatibus ea quam.
-            </div>
+    <!-- Plan your calendar -->
+    <section class="text-center bg-blue-1">
+      <div class="row">
+        <div class="col-10 col-sm-8 col-md-7 q-mx-auto">
+          <div class="text-h6 text-weight-medium q-mb-sm">
+            Planifica tu calendario
+          </div>
 
-            <q-btn
-              color="primary"
-              label="Calendario"
-              padding="sm md"
-              unelevated
-              no-caps
-              class="text-body1"
-            />
+          <div class="text-body2 q-mb-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed numquam
+            necessitatibus ea quam.
+          </div>
+
+          <q-btn
+            color="primary"
+            label="Calendario"
+            padding="sm md"
+            :ripple="false"
+            unelevated
+            no-caps
+            class="text-body2 text-weight-medium"
+          />
+        </div>
+      </div>
+    </section>
+    <!-- / Plan your calendar -->
+
+    <!-- Adventages of use SiRAl -->
+    <section class="q-px-md">
+      <div class="row q-mb-lg">
+        <div class="col-12">
+          <div class="text-h6 text-center text-weight-medium">
+            Ventajas de usar SiRAl
           </div>
         </div>
-      </section>
+      </div>
 
-      <section class="q-px-md q-pt-xl">
-        <div class="row q-mb-md">
-          <div class="col-12">
-            <div class="text-h5 text-center text-weight-medium q-pb-md">
-              Ventajas de usar SiRAl
-            </div>
-          </div>
+      <div class="row q-gutter-y-lg">
+        <div
+          v-for="({ title, subtitle, icon }, key) of benefits"
+          :key="`home-page-benefit-${key}`"
+          class="col-12 col-sm-6 col-md-4"
+        >
+          <q-card class="text-center" flat>
+            <q-card-section class="q-py-sm">
+              <q-avatar
+                :icon="icon.name"
+                font-size="4rem"
+                size="5rem"
+                :text-color="icon.color"
+                class="q-mb-md"
+              />
+              <div class="text-body1 text-weight-medium">{{ title }}</div>
+            </q-card-section>
+            <q-card-section class="q-py-none">
+              <div class="text-body2">
+                {{ subtitle }}
+              </div>
+            </q-card-section>
+          </q-card>
         </div>
-
-        <div class="row q-gutter-y-xl">
-          <div
-            v-for="({ title, subtitle, icon }, key) of benefits"
-            :key="`home-page-benefit-${key}`"
-            class="col-12 col-sm-4"
-          >
-            <q-card class="text-center" flat>
-              <q-card-section class="q-pt-xs q-pb-none">
-                <q-avatar
-                  :icon="icon.name"
-                  font-size="4rem"
-                  size="5rem"
-                  :text-color="icon.color"
-                  class="q-mb-md"
-                />
-                <div class="text-h6">{{ title }}</div>
-              </q-card-section>
-              <q-card-section class="q-py-xs">
-                <div class="text-body2">
-                  {{ subtitle }}
-                </div>
-              </q-card-section>
-            </q-card>
-          </div>
-        </div>
-      </section>
-    </div>
+      </div>
+    </section>
+    <!-- / Adventages of use SiRAl -->
   </q-page>
 </template>
 

ed11e5e98a2c0a787c63c774bd62384eb9589dfc
dcq <dariancapoteq@gmail.com>
2022-08-29 09:56:39 -0400

    Improve design, structure and styles.

--- src/components/layouts/MainFooter.vue
+++ src/components/layouts/MainFooter.vue
@@ -1,8 +1,10 @@
 <template>
-  <q-footer bordered class="q-py-sm" style="position: relative">
-    <q-toolbar class="text-subtitle2 text-center q-py-sm block">
-      <div class="text-uppercase text-weight-bold">unica-uclv</div>
-      Todos los derechos reservados <br />
+  <q-footer bordered class="q-py-md" style="position: relative">
+    <q-toolbar class="text-subtitle1 justify-center">
+      <div class="col-12 col-md-8 text-center">
+        <div class="text-uppercase text-weight-bold">unica-uclv</div>
+        Todos los derechos reservados
+      </div>
     </q-toolbar>
   </q-footer>
 </template>

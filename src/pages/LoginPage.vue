ea59f6b8708ffe66aefb79e64a694f59b3312323
dcq <dariancapoteq@gmail.com>
2022-08-29 09:59:57 -0400

    Change styles and use setup composition API.

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,23 +1,26 @@
 <template>
-  <q-page padding class="bg-grey-3">
-    <div style="padding: 6rem 0.5rem">
+  <q-page padding>
+    <div class="container">
       <q-card bordered class="q-mx-auto q-py-md" style="max-width: 350px" flat>
         <q-card-section class="text-center">
-          <div class="text-h6">Bienvenido a SIRAl-UCLV</div>
-          <div class="text-body2 text-grey-7">Inicia sesión para continuar</div>
+          <div class="text-body1 text-weight-bold">Bienvenido a SIRAl-UCLV</div>
+          <div class="text-subtitle1 text-grey-8">
+            Inicia sesión para continuar
+          </div>
         </q-card-section>
 
+        <!-- Submit form -->
         <q-form @submit="onSubmit">
           <q-card-section class="q-gutter-y-md q-mb-sm">
             <q-input v-model="user" label="Usuario" autofocus dense>
               <template v-slot:before>
-                <q-icon name="person" color="dark" />
+                <q-icon name="person" color="primary" />
               </template>
             </q-input>
 
             <q-input v-model="password" label="Contraseña" dense>
               <template v-slot:before>
-                <q-icon name="key" color="dark" />
+                <q-icon name="key" color="primary" />
               </template>
             </q-input>
           </q-card-section>
@@ -26,35 +29,32 @@
               type="submit"
               color="primary"
               padding="sm md"
-              class="text-body1 full-width"
+              :ripple="false"
+              class="text-body2 text-weight-medium full-width"
               unelevated
               no-caps
               >Entrar</q-btn
             >
           </q-card-actions>
         </q-form>
+        <!-- / Submit form -->
       </q-card>
     </div>
   </q-page>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from 'vue';
+<script setup lang="ts">
+import { ref } from 'vue';
 
-export default defineComponent({
-  name: 'LoginPage',
-  setup() {
-    const user = ref(null);
-    const password = ref(null);
-
-    return {
-      user,
-      password,
-
-      onSubmit() {
-        console.log('Submit');
-      },
-    };
-  },
-});
+const user = ref(null);
+const password = ref(null);
+const onSubmit = () => {
+  console.log('Submit');
+};
 </script>
+
+<style lang="scss">
+.container {
+  padding: 6rem 0.5rem;
+}
+</style>

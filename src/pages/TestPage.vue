22c0fa0e9bd9b011d740de2488a7479658b82f4f
dcq <dariancapoteq@gmail.com>
2022-08-29 14:18:06 -0400

    Improve design. Add normal and small buttons.

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -1,7 +1,9 @@
 <template>
   <q-page padding>
-    <div class="q-pa-md">
+    <div class="q-pa-md q-gutter-y-md">
+      <!-- Colors -->
       <div class="row q-gutter-y-md">
+        <div class="text-h6 text-bold">Colors</div>
         <div class="col-12 q-gutter-sm">
           <q-btn
             v-for="(color, index) of theme"
@@ -14,29 +16,62 @@
             unelevated
           />
         </div>
+      </div>
+
+      <div class="row q-gutter-y-md">
+        <div class="text-h6 text-bold">Cards by colors</div>
         <div class="col-12 q-gutter-md">
           <q-card
             v-for="(color, index) of theme"
             :key="`test-page-card-${index}`"
             :class="`bg-${color}`"
             dark
+            flat
           >
             <q-card-section>
               <div class="text-h6">{{ color }}</div>
-              <div class="text-subtitle2">by John Doe</div>
+              <div class="text-subtitle2">Subtitle</div>
             </q-card-section>
-            <q-card-section>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit
-            </q-card-section>
+            <q-card-section> Body </q-card-section>
           </q-card>
         </div>
+      </div>
+      <!-- / Colors -->
 
-        <div class="col-12 q-gutter-sm">
+      <!-- Buttons -->
+      <div class="row q-gutter-y-md">
+        <div class="text-h6 text-bold">Buttons</div>
+        <div class="col-12 q-gutter-md">
           <q-btn
             color="primary"
             label="Hello World"
             :ripple="false"
             size="md"
+            padding="sm md"
+            no-caps
+            unelevated
+            class="text-body2 text-weight-medium"
+          />
+
+          <q-btn
+            color="primary"
+            label="Hello World"
+            :ripple="false"
+            size="md"
+            padding="sm md"
+            no-caps
+            outline
+            unelevated
+            class="text-body2 text-weight-medium"
+          />
+        </div>
+
+        <div class="col-12 q-gutter-md">
+          <q-btn
+            color="primary"
+            label="Hello World"
+            :ripple="false"
+            padding="xs sm"
             no-caps
             unelevated
           />
@@ -45,13 +80,14 @@
             color="primary"
             label="Hello World"
             :ripple="false"
-            size="md"
+            padding="xs sm"
             no-caps
             outline
             unelevated
           />
         </div>
       </div>
+      <!-- / Buttons -->
     </div>
   </q-page>
 </template>

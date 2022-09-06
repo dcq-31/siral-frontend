e4e336ba7ef72a7798e6d43d1caba4d8d4389383
dcq <dariancapoteq@gmail.com>
2022-09-06 15:46:39 -0400

    Update event handler for toggle drawer.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <MainHeader @toggle:drawerState="toggleDrawer" />
+    <MainHeader @update:drawer-state="toggleDrawer" />
     <MainDrawer v-model:drawer-state="drawerState" />
 
     <q-page-container>
@@ -13,11 +13,10 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-// Layout components
 import MainHeader from 'components/layouts/MainHeader.vue';
 import MainFooter from 'components/layouts/MainFooter.vue';
 import MainDrawer from 'components/layouts/MainDrawer.vue';
 
 const drawerState = ref(false);
-const toggleDrawer = () => (drawerState.value = !drawerState.value);
+const toggleDrawer = (value: boolean) => (drawerState.value = value);
 </script>
